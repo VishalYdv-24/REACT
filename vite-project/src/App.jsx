0e2790bceacf9c7c06af2929a9ae37c1982d58ff
Vishal <vishal2004500@gmail.com>
2025-01-07 20:40:38 +0530

    Name Input Form

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,30 +2,39 @@ import { useState } from 'react'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  let inc = () => {
-    setCount(count+1)
+  const [inputValue,setInputValue] = useState('');
+  const [isClicked,setIsClicked] = useState(false);
+  const [store, setStore] = useState('');
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    console.log(newValue);
   }
 
-  let reset = () => {
-    setCount(count-count)
+  const submitEffect = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false)
+    }, 200);
   }
-  let dec = () => {
-    setCount(count-1)
-}
+  const isSubmit = () => {
+    submitEffect();
+    const newInput = inputValue;
+    setStore(newInput);
+    console.log(`Name : ${newInput}`);
+  }
   return (
     <>
-    <div className='flex justify-center items-center h-dvh bg-slate-300'>
-      <div className='flex justify-center items-center w-1/2 h-1/2 bg-fuchsia-100 rounded-xl flex-col gap-y-6'>
-        <div>
-          <h1 className='text-5xl'>Counter : {count}</h1>
-        </div>
-        <div className=''>
-          <button onClick={inc} className='bg-black text-white text-lg mx-1 p-1 px-2 rounded-lg'>Increase</button>
-          <button onClick={dec} className='bg-black text-white text-lg mx-1 p-1 px-2 rounded-lg'>Decrease</button>
-          <button onClick={reset} className='bg-black text-white text-lg mx-1 p-1 px-2 rounded-lg'>Reset</button>
-        </div>
+    <div className='flex flex-col-reverse border-2 border-black h-dvh w-full justify-center items-center bg-green-200 '>
+      <div className='flex bg-slate-200 w-5/12 gap-x-2 px-2'>
+          <p className='font-bold '>Name : </p>
+          <input type="text" value={inputValue} onChange={handleInputChange} placeholder='Enter Name' className=' border-b-2 border-black h-1/8 bg-slate-200 w-3/4' />
+          <button onClick={isSubmit} value="Vishal"className={`px-2 ml-4 text-white font-bold ${isClicked? "bg-green-400" : "bg-sky-500"} hover:${isClicked? "bg-green-400" : "bg-sky-700"}` }>Submit</button>
+      </div>
+      <div>
+          {store && <h1> Name : {store}</h1>}
       </div>
     </div>
     </>
@@ -33,3 +42,4 @@ function App() {
 }
 
 export default App
+
