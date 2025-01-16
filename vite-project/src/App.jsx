223522fe319bc3197f42801da33cfeae6f3b4ec3
Vishal <vishal2004500@gmail.com>
2025-01-16 22:40:36 +0530

    UseEffect()

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const[color,setColor] = useState("RED");
-  const changeColor = ()=>{
-    setColor("BLUE")
-  }
+  /*
+  SYNTAX :
+  useEffect(callBack function,dependency)
+  useEffect(() => {
+    callBack function code
+    },[])
 
-  // USESTATE USING OBJECT 
-  const[bike,setBike] = useState({
-    brand:"Honda",
-    color:"Black",
-    model:"Unicorn",
-    year:"2021",
-  })
+  
+  
+  */
 
-  const changeBike = () => {
-    setBike((prev) => {
-      return{...prev,brand:"Hero",
-        color:"Red",
-        model:"Passion Pro",
-        year:"2019"}
+    const[count,setCount]=useState(0)
+    const[empty,setEmpty]=useState(0)
+    const[vari,setVari]=useState(0)
+
+
+// without any dependency it will execute whenever the state is chnaged
+    useEffect(()=>{
+      setTimeout(() => {
+        setCount(count=>count+1)
+      }, 2000);
     })
-  }
 
-  const [count,setCount]=useState(0)
-  const incCount = () => {
-    setCount((prev) => prev+1)
-    setCount((prev) => prev+1)
-    setCount((prev) => prev+1)
-    setCount((prev) => prev+1)
-  }
+// with empty dependency array it will execute only onces irrespective of chnage in state
+useEffect(()=>{
+  setTimeout(() => {
+    setEmpty(count=>count+1)
+  }, 2000);
+},[])
+
+// with variable it will change whenever variable chnages
+  useEffect(()=>{
+    setTimeout(() => {
+      setVari(count=>count+1)
+    }, 2000);
+  },[vari])
   return (
     <>
     <div className='flex justify-center items-center flex-col gap-y-10'>
-      <div className='text-center text-9xl'>USESTATE</div>
-      <h1 className='text-3xl'>My Favorite Color is {color}!</h1>
-      <button onClick={changeColor}>BLUE</button>
-
-
-      <h1>My {bike.brand}, It is {bike.color} {bike.model} from year {bike.year}</h1>
-      <button onClick={changeBike}>Change</button>
-
-      <h1>COUNT: {count}</h1>
-      <button onClick={incCount}>INC by 4</button>
+      <div className='text-center text-5xl'>useEffect</div>
+      <h1 className='font-bold text-3xl'>without D: I've render {count} times</h1>
+      <h1 className='font-bold text-3xl'>empty D: I've render {empty} times</h1>
+      <h1 className='font-bold text-3xl'>var D: I've render {vari} times</h1>
     </div>
 
     </>
