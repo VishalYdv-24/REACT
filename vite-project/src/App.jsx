dbb3cdd24c105bb2d15c0d192bc481eba4bd6f68
Vishal <vishal2004500@gmail.com>
2025-01-19 13:14:26 +0530

    Background Color Changer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,52 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  /*
-  SYNTAX :
-  useEffect(callBack function,dependency)
-  useEffect(() => {
-    callBack function code
-    },[])
-
-  
-  
-  */
-
-    const[count,setCount]=useState(0)
-    const[empty,setEmpty]=useState(0)
-    const[vari,setVari]=useState(0)
-
-
-// without any dependency it will execute whenever the state is chnaged
-    useEffect(()=>{
-      setTimeout(() => {
-        setCount(count=>count+1)
-      }, 2000);
-    })
-
-// with empty dependency array it will execute only onces irrespective of chnage in state
-useEffect(()=>{
-  setTimeout(() => {
-    setEmpty(count=>count+1)
-  }, 2000);
-},[])
-
-// with variable it will change whenever variable chnages
-  useEffect(()=>{
-    setTimeout(() => {
-      setVari(count=>count+1)
-    }, 2000);
-  },[vari])
+    const [color,setColor] = useState('pink')
+    const colorArr = ["red","green","blue","pink","brown","orange","yellow","lime","purple"]
   return (
-    <>
-    <div className='flex justify-center items-center flex-col gap-y-10'>
-      <div className='text-center text-5xl'>useEffect</div>
-      <h1 className='font-bold text-3xl'>without D: I've render {count} times</h1>
-      <h1 className='font-bold text-3xl'>empty D: I've render {empty} times</h1>
-      <h1 className='font-bold text-3xl'>var D: I've render {vari} times</h1>
+    <div className='bg-slate-500 h-screen w-screen' style={{backgroundColor:color}}>
+        <div className='w-10/12 h-11 bg-slate-700 m-auto rounded-xl flex justify-center items-center'>
+            {
+                colorArr.map((clr) => (
+                    <>
+                    <div className='m-auto'>
+                        <button className='text-white m-auto p-1 rounded-sm' style={{backgroundColor:clr}} onClick={()=> setColor(clr)}>{clr}</button>
+                    </div>
+                    </>
+                ))
+            }
+            
+        </div>
     </div>
-
-    </>
   )
 }
 
